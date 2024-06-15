bf42d1ff40a3db7a5a7c6bda8c9b4728230ed0f5
Sotiraq <rai.xha2004@icloud.com>
2024-06-15 19:59:36 +0200

    Add files via upload

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+mysqli_report(MYSQLI_REPORT_OFF);
+$conn = mysqli_connect('localhost','root','','user_db');
+
+?>
---
+++ user-db.sql
@@ -0,0 +1,41 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Pritësi (host): 127.0.0.1
+-- Koha e gjenerimit: Jan 22, 2024 në 09:30 PD
+-- Versioni i serverit: 10.4.32-MariaDB
+-- PHP Versioni: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Databaza: `user-db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura e tabelës për tabelën `user_form`
+--
+
+CREATE TABLE `user_form` (
+  `id` int(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `user_type` varchar(255) NOT NULL DEFAULT 'user'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
